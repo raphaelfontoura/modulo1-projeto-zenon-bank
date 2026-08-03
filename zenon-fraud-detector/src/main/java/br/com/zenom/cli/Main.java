301ab403194d39e527b2c3cb235d2b16d3ba83e1
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-08-03 20:43:12 -0300

    Limit print to max 10 lines.

--- zenon-fraud-detector/src/main/java/br/com/zenom/cli/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/cli/Main.java
@@ -21,7 +21,7 @@ public class Main {
         TransactionIngestor ingestor = new TransactionIngestor();
         try {
             List<Transaction> transactions = ingestor.ingestorFileTransactions("data/PS_20174392719_1491204439457_log.csv");
-            System.out.println(transactions);
+            transactions.stream().limit(10).forEach(System.out::println);
         } catch (IOException ex) {
             log.log(Level.SEVERE, "Falha ao ler arquivo", ex);
         }
