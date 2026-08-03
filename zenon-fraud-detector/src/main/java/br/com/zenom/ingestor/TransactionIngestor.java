0d10d96f0c809426fa3ce6abdc798b75ec7a1de0
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-08-03 20:53:30 -0300

    Refactor ingestorFileTransactions to use stream.

--- zenon-fraud-detector/src/main/java/br/com/zenom/ingestor/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/ingestor/TransactionIngestor.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionIngestor {
@@ -16,20 +15,21 @@ public class TransactionIngestor {
     public List<Transaction> ingestorFileTransactions(String fileName) throws IOException {
         Path path = Path.of(fileName);
         List<String> transactionLines = Files.readAllLines(path);
-        List<Transaction> transactions = new ArrayList<>();
-        for (int i = 1; i <= 1000 ; i++) {
-            String[] values = transactionLines.get(i).split(",");
-            int step = Integer.parseInt(values[0]);
-            TransactionType type = TransactionType.valueOf(values[1]);
-            BigDecimal amount = new BigDecimal(values[2]);
-            Customer orig= new Customer(values[3], new BigDecimal(values[4]), new BigDecimal(values[5]));
-            Customer recipient = new Customer(values[6], new BigDecimal(values[7]), new BigDecimal(values[8]));
-            boolean isFraud = values[9].equals("1");
-            boolean isFlagged = values[10].equals("1");
-            Transaction transaction = new Transaction(step, type, amount, orig, recipient, isFraud, isFlagged);
-            transactions.add(transaction);
-        }
+        return transactionLines.stream()
+                .skip(1)
+                .limit(1000)
+                .map(line -> getTransaction(line.split(",")))
+                .toList();
+    }
 
-        return transactions;
+    private static Transaction getTransaction(String[] values) {
+        int step = Integer.parseInt(values[0]);
+        TransactionType type = TransactionType.valueOf(values[1]);
+        BigDecimal amount = new BigDecimal(values[2]);
+        Customer orig= new Customer(values[3], new BigDecimal(values[4]), new BigDecimal(values[5]));
+        Customer recipient = new Customer(values[6], new BigDecimal(values[7]), new BigDecimal(values[8]));
+        boolean isFraud = values[9].equals("1");
+        boolean isFlagged = values[10].equals("1");
+        return new Transaction(step, type, amount, orig, recipient, isFraud, isFlagged);
     }
 }
