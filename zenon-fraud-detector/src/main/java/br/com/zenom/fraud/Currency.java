4b381676ddd3680ff0d4d053bceef79b4e57422d
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-08-03 23:30:10 -0300

    Refactor validation of Currency to use signum.

--- zenon-fraud-detector/src/main/java/br/com/zenom/fraud/Currency.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/fraud/Currency.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public record Currency(BigDecimal amount) {
     public Currency(String amount) {
         this(new BigDecimal(amount));
-        if (this.amount.compareTo(BigDecimal.ZERO) < 0)
+        if (this.amount.signum() < 0)
             throw new IllegalArgumentException("amount should be positive: " + amount);
     }
 }
