8989b56c8fde4e416940068a3ba500e4f0e54dd0
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-08-07 15:23:38 -0300

    feat: implement i18n.

--- zenon-fraud-detector/src/main/java/br/com/zenom/report/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/report/ReportMain.java
@@ -1,16 +1,30 @@
 package br.com.zenom.report;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ReportMain {
 
     static void main() {
         TransactionReport transactionReport = new TransactionReport("data/PS_20174392719_1491204439457_log.csv");
         TransactionReport.Statistics statistics = transactionReport.generateReport();
 
+        Locale locale = Locale.of("pt", "BR");
+//        Locale locale = Locale.US;
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("reportmessages", locale);
+
         IO.println("""
-                Total de linhas: %d
-                Total de fraudes: %d
-                Valor total das transações: %.2f
-                """.formatted(statistics.totalTransactions(), statistics.totalFrauds(), statistics.totalAmount()));
+                %s: %s
+                %s: %s
+                %s: %s
+                """.formatted(
+                        resourceBundle.getString("total.lines"), numberFormat.format(statistics.totalTransactions()),
+                        resourceBundle.getString("total.frauds"),numberFormat.format(statistics.totalFrauds()),
+                        resourceBundle.getString("total.amount"), numberFormat.format(statistics.totalAmount())
+                )
+        );
 
     }
 }
