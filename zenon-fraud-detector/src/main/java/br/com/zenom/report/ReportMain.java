16493c9c584756f59f12748c78798a4fa510571b
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-08-07 00:38:08 -0300

    refactor: Refactor TransactionReport and optimized to use one stream call.

--- zenon-fraud-detector/src/main/java/br/com/zenom/report/ReportMain.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/report/ReportMain.java
@@ -1,31 +1,16 @@
 package br.com.zenom.report;
 
-import br.com.zenom.ingestor.TransactionMap;
-
-import java.math.BigDecimal;
-import java.util.Optional;
-
 public class ReportMain {
 
     static void main() {
         TransactionReport transactionReport = new TransactionReport("data/PS_20174392719_1491204439457_log.csv");
+        TransactionReport.Statistics statistics = transactionReport.generateReport();
 
-        long totalLines = transactionReport.loadTransactionsFile().skip(1).count();
-
-        IO.println("Total de linhas: " + totalLines);
-
-        long totalFrauds = transactionReport.loadTransactionsFile().skip(1).map(TransactionMap::getTransaction)
-                .filter(t -> t.get().isFraud())
-                .count();
-        IO.println("Total de fraudes: " + totalFrauds);
-
-        BigDecimal totalTransactions = transactionReport.loadTransactionsFile().skip(1)
-                .map(TransactionMap::getTransaction)
-                .filter(Optional::isPresent)
-                .map(transaction -> transaction.get().amount().value())
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        IO.println("Valor total transacionado: " + totalTransactions.toPlainString());
+        IO.println("""
+                Total de linhas: %d
+                Total de fraudes: %d
+                Valor total das transações: %.2f
+                """.formatted(statistics.totalTransactions(), statistics.totalFrauds(), statistics.totalAmount()));
 
     }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenom/report/TransactionReport.java
+++ zenon-fraud-detector/src/main/java/br/com/zenom/report/TransactionReport.java
@@ -1,23 +1,69 @@
 package br.com.zenom.report;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class TransactionReport {
 
     private final Path filePath;
+    private static final long HEADER_NUMBER = 1;
 
     public TransactionReport(String filePath) {
+        Objects.requireNonNull(filePath);
         this.filePath = Path.of(filePath);
     }
 
-    public Stream<String> loadTransactionsFile() {
-        try {
-           return Files.lines(filePath);
+    public record ReportTransaction(BigDecimal amount, boolean isFraud) {
+    }
+
+    public record Statistics (long totalTransactions, long totalFrauds, BigDecimal totalAmount) {
+        private static Statistics STATISTICS_ZERO() {
+            return new Statistics(0, 0, BigDecimal.ZERO);
+        }
+
+        private Statistics addTransaction(ReportTransaction reportTransaction) {
+            return new Statistics(totalTransactions() + 1,
+                    totalFrauds() + (reportTransaction.isFraud ? 1 : 0),
+                    totalAmount().add(reportTransaction.amount)
+            );
+        }
+
+        private Statistics add(Statistics other) {
+            return new Statistics(
+                    totalTransactions + other.totalTransactions,
+                    totalFrauds + other.totalFrauds,
+                    totalAmount.add(other.totalAmount));
+        }
+    }
+
+    public Statistics generateReport() {
+        try(Stream<String> lines = Files.lines(filePath)) {
+            return lines.skip(HEADER_NUMBER)
+                    .map(TransactionReport::getReportTransaction)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .reduce(Statistics.STATISTICS_ZERO(), Statistics::addTransaction, Statistics::add);
         } catch (IOException ex) {
             throw new RuntimeException("Falha ao ler arquivo", ex);
         }
     }
+
+    public static Optional<ReportTransaction> getReportTransaction(String line) {
+        String[] values = line.split(",");
+
+        try {
+            BigDecimal amount = new BigDecimal(values[2]);
+            boolean isFraud = values[9].equals("1");
+            return Optional.of(new ReportTransaction(amount, isFraud));
+        } catch (IllegalArgumentException ex) {
+            System.err.printf("Erro: %s | %s: %s \r\n", line, ex.getClass(), ex.getMessage());
+        }
+        return Optional.empty();
+    }
+
 }
